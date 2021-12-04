0ecb8e69ae4c955f6958b0c3c2caa90ea2bc37d9
Will C <2willaddiction@gmail.com>
2021-12-04 18:54:49 -0500

    Add Question and Choice Models and update Admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 8
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice.question.through
+    extra = 4
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -17,10 +23,12 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +36,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
